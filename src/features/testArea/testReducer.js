632587857bfce0c480d13a8025d059d65d7b12b8
knromaric <nzekeng@gmail.com>
2019-02-24 19:17:30 +0200

    Refactoring reducer

--- src/features/testArea/testReducer.js
+++ src/features/testArea/testReducer.js
@@ -2,26 +2,45 @@ import {
   INCREMENT_COUNTER,
   DECREMENT_COUNTER
 } from './testContants';
+import {createReducer} from './../../App/common/util/reducerUtil' 
+
+
 
 const initialState = {
   data: 555
 }
 
-const testReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT_COUNTER:
-      return {
-        ...state,
-        data: state.data + 1
-      };
-    case DECREMENT_COUNTER:
-      return {
-        ...state,
-        data: state.data - 1
-      };
-    default:
-      return state;
-  }
+export const incrementCounter = (state, payload) => {
+  return {
+    ...state,
+    data: state.data + 1
+  };
+}
+export const decrementCounter = (state, payload) => {
+  return {
+    ...state,
+    data: state.data - 1
+  };
 }
 
-export default testReducer;
+// const testReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT_COUNTER:
+//       return {
+//         ...state,
+//         data: state.data + 1
+//       };
+//     case DECREMENT_COUNTER:
+//       return {
+//         ...state,
+//         data: state.data - 1
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+export default createReducer(initialState, {
+  [INCREMENT_COUNTER]:incrementCounter, 
+  [DECREMENT_COUNTER]: decrementCounter
+});
